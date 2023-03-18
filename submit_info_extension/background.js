7e2f38a7b0eb450e39aa9ad7bac0c5ebf45bef73
Hojung Kim <hojung1996@gmail.com>
2023-03-18 12:23:12 -0500

    Got extension to display different HTML on different sites

---
+++ submit_info_extension/background.js
@@ -0,0 +1,35 @@
+const domains = ['target.com', 'amazon.com'];
+
+// // In case something breaks later, you can always use this to make just the new tabs work :)
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    // setTimeout(() => {  console.log("working at all?"); }, 3000);
+  if (changeInfo.url) {
+    if (domains.some(domain => changeInfo.url.includes(domain))) {
+      chrome.action.setPopup({ popup: 'index_state.html', tabId: tab.id });
+    } else {
+      chrome.action.setPopup({ popup: 'sleep_state.html', tabId: tab.id });
+    }
+  }
+});
+
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    let currentUrl = tabs[0].url;
+    chrome.action.setPopup({ popup: `popup.html?currentUrl=${currentUrl}` });
+});
+
+chrome.tabs.onActivated.addListener(async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    if (tab && domains.some(domain => tab.url.includes(domain))) {
+      chrome.action.setPopup({ popup: 'index_state.html' });
+    } else {
+      chrome.action.setPopup({ popup: 'sleep_state.html' });
+    }
+  });
+  
+  // Update popup when the extension is installed or updated
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.setPopup({ popup: 'sleep_state.html' });
+  });
+
+// Apparently it is running background.js
+// setTimeout(() => {  console.log("working at all?"); }, 3000);
